30353f28c8e1f44d1cbb61d90a984f4744a56b35
陈赢鹏 <vincenteof@qq.com>
2022-08-07 00:09:14 +0800

    chore: deptree package rollup config

--- packages/deptree/src/utils.ts
+++ packages/deptree/src/utils.ts
@@ -1,6 +1,6 @@
 import { readFile as _readFile } from 'fs'
 import { promisify } from 'util'
-import { parse as _parse, TransformOptions, ParseResult } from '@babel/core'
+import { parse as _parse, ParseResult, TransformOptions } from '@babel/core'
 import { basename, extname } from 'path'
 
 export const readFile = promisify(_readFile)
